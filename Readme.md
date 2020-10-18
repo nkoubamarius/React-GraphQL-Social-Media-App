752ceda4df24ce1180f21653c50a1ea7161bff50
nkoubamarius <nkoubamarius@gmail.com>
2020-10-19 00:01:04 +0100

    Add readme

---
+++ Readme.md
@@ -0,0 +1,13 @@
+## Social Media
+
+## Demo
+
+https://focused-rosalind-ea2ee0.netlify.app/
+
+## Home Page
+
+<img src="home.png">
+
+## Post Page
+
+<img src="post.png">
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const {MONGODB}=require('./config');
 
 const pubsub=new PubSub();
 
+const PORT= process.env.port || 5000;
+
 const server=new ApolloServer({
     typeDefs,
     resolvers,
@@ -15,7 +17,9 @@ const server=new ApolloServer({
 
 
 mongoose.connect(MONGODB,{useNewUrlParser: true,  useUnifiedTopology: true }).then(()=>{
-    return server.listen({port: 5000});
+    return server.listen({port: PORT});
 }).then(res=>{
     console.log(`Server running at ${res.url}`)
+}).catch(err=>{
+    console.error(err);
 })
